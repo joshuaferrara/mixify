72542844913a52c3f1dc159980503fcebd97bad7
Joshua Ferrara <scjosh2@gmail.com>
2019-05-15 17:54:22 -0700

    implemented click tag to search

--- src/js/mixify.js
+++ src/js/mixify.js
@@ -348,33 +348,18 @@ class Mixify {
         $(".fa-heart").click((event) => {
           this.toggleFavorite($(event.target).attr('id').replace('fav-', ''));
         });
-        $(".badge-primary").click(function() {
-          var key = $("#tagButton").text();
+
+        $(".badge-primary").click((event) => {
+          var key = $(event.target).text();
 
           var ingredientName = key;
-          this.searchIngredients = $('input').tagsinput({
-              typeaheadjs: {
-              source: ingredientName
-             }
-          })[0];
-         this.searchIngredients.add(ingredientName);
+          this.searchIngredients.add(ingredientName);
 
-         $('input').on('beforeItemAdd', (event) => {
-            // Cancel the item add if the ingredient the user is trying to add
-            // does not exists in our ingredient database.
-            event.cancel = this.cocktailDb.ingredients.indexOf(event.item) == -1;
-         });
-
-         let tagInputChangeEventHandler = () => {
-             this.filterCards();
-             this.sortCards();
-             this.nextPage(1);
-             this.generateCards();
-             this.displayCards();
-         }
-
-         $('input').on('itemAdded', tagInputChangeEventHandler);
-         $('input').on('itemRemoved', tagInputChangeEventHandler);
+          this.filterCards();
+          this.sortCards();
+          this.nextPage(1);
+          this.generateCards();
+          this.displayCards();
       });
     }
     nextPage(pgNum){
